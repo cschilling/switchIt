6f83b5da13c482d25f25a1ec978bc97ff0bd12b3
Christian Schilling <info@schilling-it-solutions.de>
2013-07-30 18:05:54 +0200

    Wochentage mit einbezogen, ob ein Cronjob ausgeführt werden soll

--- web/cron.php
+++ web/cron.php
@@ -4,10 +4,11 @@
 	$aData = json_decode(file_get_contents(__DIR__."/data.json"), true);
 	$time  = (date('H') * 60) + (date('i'));
 
-	foreach($aData['cronjobs'] AS $cron)
+	foreach($aData['cronjobs'] AS $cKey => $cron)
 	{
-		if ($time == $cron['time'])
+		if (substr($cron['days'], date('N') -1, 1) == 1 && $time == $cron['time'])
 		{
+			echo "excuting cronjob #".$cKey."\n";
 			foreach($cron['switches'] AS $switchKey => $action)
 			{
 				$switch = $aData['switches'][$switchKey];
