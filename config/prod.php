5e4b1756381040f37d4e5fc3e849750044afd861
Christian Schilling <info@schilling-it-solutions.de>
2013-08-01 16:40:24 +0200

    version hardcoded

--- config/prod.php
+++ config/prod.php
@@ -5,4 +5,3 @@ $sendBin        = '/opt/raspberry-remote/send';
 $dataFile       = __DIR__.'/../data/data.json';
 $switchFile     = __DIR__.'/../data/toSwitch.json';
 $logFile        = __DIR__.'/../logs/switchIt.log';
-$app['version'] = '0.9';
